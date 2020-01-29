a46fd431c49d0fb562678d1950eb627988f6a416
Qi Yin <1052317189@qq.com>
2020-01-28 21:32:56 -0500

    Revert "add a new file"
    This reverts commit 632393ed0b74368cbc6f1ada4f9e9ed926c02d52.
    
    Add function
    
    Substract function

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,13 @@
 def hello_world():
     print("Hello World!")
 
+def add(x, y):
+    return x + y
+
+def substract(x, y):
+    return x - y
+
 hello_world()
+print(add(1, 1))
+print(substract(3, 1))
+
